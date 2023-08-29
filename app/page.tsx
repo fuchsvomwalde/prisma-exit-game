d65d1e4e4c2a40651f9479833191527a235afefd
Jascha A. Quintern <jascha.quintern@inf.works>
2023-08-29 22:00:35 +0200

    fix Missing "key" prop for element in iterator  react/jsx-key

--- app/page.tsx
+++ app/page.tsx
@@ -49,6 +49,7 @@ export default async function Home() {
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
           {games.map((game) => (
             <NavTile
+              key={game.slug}
               href={`/${game.slug}`}
               title={game.name}
               subline={game.description}
